5e22b7e151a5fabd5f35b112bf97c97fe19a5248
pvpatil5 <pavan@freshprints.com>
2023-12-20 08:08:05 +0530

    Commit message

--- src/test/java/demo2/Demo4.java
+++ src/test/java/demo2/Demo4.java
@@ -1,12 +1,10 @@
 package demo2;
 
-public class Demo2 {
+public class Demo4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Demo2");
-		
-		System.out.println("New Line added");
+		System.out.println("Demo4");
 	}
 
 }
